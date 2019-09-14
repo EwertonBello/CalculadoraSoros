443491e48b9edf2239065103de4f2dcd91382cbe
EwertonBello <ewerton.bello1212@gmail.com>
2019-09-13 21:32:50 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'calculadora-soros';
+export class AppComponent 
+{
+	public investimento: number = 0;  
+	public payout: number = 0;  
+	public soros: number = 100;  
+	public niveis: number = 4;  
+	public resultados: string = '';  
+	private valor: number;
+
+
+	calcular()
+	{
+		this.valor = this.investimento
+		for (let i = 0; i < this.niveis; i++) 
+		{
+			this.valor+=((this.valor*(this.payout/100))*(this.soros/100))
+			this.resultados+=this.valor.toFixed(2)+'\n'
+		}
+	}
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatGridListModule,
+    MatToolbarModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
